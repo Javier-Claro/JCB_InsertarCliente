f6d04c79fdd2df7dd4ae6f09e0abd0bc9f05888f
DESKTOP-D5NS5P4\manuel <manuelclarobarba@gmail.com>
2023-11-17 18:29:52 +0100

    no message

--- JCB_InsertarCliente/Servicios/MenuItfz.cs
+++ JCB_InsertarCliente/Servicios/MenuItfz.cs
@@ -13,13 +13,11 @@ namespace JCB_InsertarCliente.Servicios
     {
         /// <summary>
         /// Método que muestra mensaje de bienvenida al cajero.
-        /// 250923-rfg
         /// </summary>
         public void mostrarMensajeBienvenida();
 
         /// <summary>
         /// Método que muestra el menú y recoge la selección del usuario
-        /// 260923-rfg
         /// </summary>
         /// <returns>un entero con la opción seleccionada</returns>
         public int mostrarMenuYSeleccion();
